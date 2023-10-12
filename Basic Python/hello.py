349fe780c96c637200d08189dee940fd66ad10ec
nh246 <nazmul210130@diit.edu.bd>
2023-10-12 13:41:17 +0600

    adding code

---
+++ Basic Python/hello.py
@@ -0,0 +1,14 @@
+print("Hellow world!")
+print("Hellow", end= ' ')
+print("world")
+
+# comments in python
+
+'''multiline comment in python'''
+#  Variables, Data Types, and Operators
+
+# Variables in python
+a= 5
+print(a)
+b=44
+print(b)
