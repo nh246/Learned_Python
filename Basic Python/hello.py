c54cfaa083caa498278b83db78f3819b6aa8c6f6
nh246 <nazmul210130@diit.edu.bd>
2023-10-12 14:23:25 +0600

    adding code

--- Basic Python/hello.py
+++ Basic Python/hello.py
@@ -1,6 +1,6 @@
-print("Hellow world!")
-print("Hellow", end= ' ')
-print("world")
+# print("Hellow world!")
+# print("Hellow", end= ' ')
+# print("world")
 
 # comments in python
 
@@ -8,7 +8,55 @@ print("world")
 #  Variables, Data Types, and Operators
 
 # Variables in python
-a= 5
-print(a)
-b=44
-print(b)
+# a= 5
+# print(a)
+# b=44
+# print(b)
+# Global Variables
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+# Use of global keyword
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+# Datatypes in Python
+
+#    Built-in Data Types
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# x = 5
+# x = 1j
+# x = ["apple", "banana", "cherry"]
+# x = ("apple", "banana", "cherry")
+# x = range(6)
+# x = {"name" : "John", "age" : 36}
+# x = {"apple", "banana", "cherry"}
+# x = frozenset({"apple", "banana", "cherry"})
+# x = True
+# x = b"Hello"
+# x = bytearray(5)
+# x = memoryview(bytes(5))
+# x = None
+
+print(type(x)) 
