9a6d0d38f5033fc4c3a77b4bfbbea2d0350b7b91
nh246 <nazmul210130@diit.edu.bd>
2023-10-27 14:07:47 +0600

    Boolean in python

--- Basic Python/Boolean.py
+++ Basic Python/Boolean.py
@@ -27,3 +27,38 @@ else:
 print(bool("Hello"))
 print(bool(15))
 
+
+# The following will return False:
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+#One more value, or object in this case, evaluates to False, and that is if you have an object that is made from a class with a __len__ function that returns 0 or False:
+
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+# You can execute code based on the Boolean answer of a function:
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+# Python also has many built-in functions that return a boolean value, like the isinstance() function, which can be used to determine if an object is of a certain data type:  
+
+x = 200
+print(isinstance(x, int))
