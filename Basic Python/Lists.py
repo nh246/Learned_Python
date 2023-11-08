fc9c668e101d51a0b388e66931ca49000254db1d
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-11-08 19:19:08 +0600

    Update Lists.py

--- Basic Python/Lists.py
+++ Basic Python/Lists.py
@@ -15,3 +15,8 @@ list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 list4 = ["abc", 34, True, 40, "male"]
+
+'''Using the list() constructor to make a List:'''
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
