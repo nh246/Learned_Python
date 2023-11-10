cbefe3db6d0fe4ce85464b8ba348535953005c64
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-11-10 22:52:24 +0600

    Update Lists.py

--- Basic Python/Lists.py
+++ Basic Python/Lists.py
@@ -23,3 +23,7 @@ print(thislist)
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
