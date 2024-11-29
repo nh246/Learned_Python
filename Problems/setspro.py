611f5033468ac4e32f1d8768ec4cd0236317eb7e
nh246 <nazmul210130@diit.edu.bd>
2024-11-29 20:45:05 +0600

    sets added

---
+++ Problems/setspro.py
@@ -0,0 +1,29 @@
+#display unique numbers
+
+s = set()
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+n = input("Enter number: ")
+s.add(int(n))  
+
+print(s)
+
+# length of s after these operations?
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') 
+# The leanth will be 2 beacuse python dosen't considers int and float as diferent it sees as same 
+print(len(s))
